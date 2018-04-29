95ce6461495d7e195935bc08e3c354e9b2b83948
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-04-29 10:29:47 +0100

    Refactor tests for expansion

--- test.js
+++ test.js
@@ -1,37 +1,33 @@
-var end = require('./')
 var pull = require('pull-stream')
+var resolve = require('./')
 var test = require('tape')
 
-test('Happy flow', async function (t) {
-  t.plan(1)
+var happy = pull(
+  pull.values([1, 2, 3]),
+  pull.asyncMap(function (val, cb) {
+    cb(null, val * 2)
+  })
+)
 
-  var stream = pull(
-    pull.values([1, 2, 3]),
-    pull.asyncMap(function (val, cb) {
-      cb(null, val * 2)
-    })
-  )
+var fail = pull(
+  pull.values([1, 2, 3]),
+  pull.asyncMap(function (val, cb) {
+    cb(new Error('panic!'))
+  })
+)
+
+test('onEnd', async function (t) {
+  t.plan(2)
 
   try {
-    await end(stream)
+    await resolve(happy)
     t.ok(true, 'no error')
   } catch (err) {
     t.notOk(err, 'this should not be executred')
   }
-})
-
-test('Throw error', async function (t) {
-  t.plan(1)
-
-  var stream = pull(
-    pull.values([1, 2, 3]),
-    pull.asyncMap(function (val, cb) {
-      cb(new Error('panic!'))
-    })
-  )
 
   try {
-    await end(stream)
+    await resolve(fail)
     t.ok(false, 'this should not be executred')
   } catch (err) {
     t.ok(err, 'error thrown')
